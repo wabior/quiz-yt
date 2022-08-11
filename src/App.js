5c6a89e9f710464653171b73b1adaed93ba770f1
Jarosaw Wabich <wabior@gmail.com>
2022-08-11 21:32:15 +0200

    episode #2 end

--- src/App.js
+++ src/App.js
@@ -3,18 +3,45 @@ import Header from "./components/Header/Header";
 import Welcome from "./components/Welcome/Welcome";
 import Quiz from "./components/Quiz/Quiz";
 import Score from "./components/Score/Score";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-        <div className={'container'}>
-            <Welcome/>
-            <Quiz/>
-            <Score/>
+    const statuses = [
+        'initial',
+        'quiz',
+        'score'
+    ];
+
+    const [appStatus, setAppStatus] = useState('initial');
+
+    const changeStatus = () => {
+        let status = statuses.indexOf(appStatus) + 1;
+        console.log(status);
+        status = status > 2 ? 0 : status;
+        console.log(status);
+        setAppStatus(statuses[status]);
+    }
+
+    return (
+        <div className="App">
+            <Header/>
+            <div className={'container'}>
+                {appStatus === 'initial' && <Welcome/>}
+                {appStatus === 'quiz' && <Quiz/>}
+                {appStatus === 'score' && <Score/>}
+
+                {!statuses.includes(appStatus) &&
+                    <div className={'alert alert-danger'}>wrong status</div>
+                }
+
+                <button
+                    onClick={changeStatus}
+                    className={'btn btn-danger my-4'}>
+                    Change status
+                </button>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
